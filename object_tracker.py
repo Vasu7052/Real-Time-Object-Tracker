746354f66d504cd07ae6b03a90c640a5bbaca42b
Vasu Gupta <androfly.official@gmail.com>
2017-10-21 13:31:43 +0530

    Combined both the methods

--- object_tracker.py
+++ object_tracker.py
@@ -14,23 +14,23 @@ while (True):
 
         res = cv2.matchTemplate(gray, template, cv2.TM_SQDIFF)
 
+        '''
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        
+        top_left = min_loc
+        bottom_right = (top_left[0] + w, top_left[1] + h)
 
-        '''top_left = min_loc
-            bottom_right = (top_left[0] + w, top_left[1] + h)
-
-            cv2.rectangle(frame, top_left, bottom_right, (0,0,255), 3)
-            cv2.putText(frame, 'Detected Pic : ' + str(i), (top_left[0], top_left[1] - 10),
-                        cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255))
+        cv2.rectangle(frame, top_left, bottom_right, (0,0,255), 3)
+        cv2.putText(frame, 'Detected Pic : ' + str(i), (top_left[0], top_left[1] - 10) , cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255))
         '''
 
-        threshold = 0.5
+        threshold = 0.8
         loc = np.where(res >= threshold)
 
         for pt in zip(*loc[::-1]):
+
             cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (255 , 0 , 0 ), 2)
-            cv2.putText(frame, 'Detected Pic : ' + str(i), (pt[0], pt[1] - 10),
-                        cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 255))
+            cv2.putText(frame, 'Detected Pic : ' + str(i), (pt[0], pt[1] - 10) , cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 255))
 
 
     cv2.imshow('Object Tracker', frame)
