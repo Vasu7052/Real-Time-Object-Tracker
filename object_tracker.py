20488633a8b06214f6bde8933585f21d482eb37e
Vasu Gupta <androfly.official@gmail.com>
2017-10-21 21:48:06 +0530

    Improved Accuracy

--- object_tracker.py
+++ object_tracker.py
@@ -8,29 +8,23 @@ while (True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    for i in range(1,3):
+    for i in range(1,2):
         template = cv2.imread('images/pic'+str(i)+'.png', 0)
         w, h = template.shape[::-1]
 
-        res = cv2.matchTemplate(gray, template, cv2.TM_SQDIFF)
+        res = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF_NORMED)
+        threshold = 0.6
 
-        '''
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-        
-        top_left = min_loc
-        bottom_right = (top_left[0] + w, top_left[1] + h)
-
-        cv2.rectangle(frame, top_left, bottom_right, (0,0,255), 3)
-        cv2.putText(frame, 'Detected Pic : ' + str(i), (top_left[0], top_left[1] - 10) , cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255))
-        '''
-
-        threshold = 0.8
         loc = np.where(res >= threshold)
 
         for pt in zip(*loc[::-1]):
-
-            cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (255 , 0 , 0 ), 2)
-            cv2.putText(frame, 'Detected Pic : ' + str(i), (pt[0], pt[1] - 10) , cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 255))
+            top_left = pt
+            bottom_right = (top_left[0] + w, top_left[1] + h)
+            demo = cv2.rectangle(frame, top_left, bottom_right, (0, 0, 255), 3)
+            cv2.putText(frame, 'Detected Pic : ' + str(i), (top_left[0], top_left[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1.0,(255, 255, 255))
+            if demo is not None :
+                print('Oject Found')
+            break
 
 
     cv2.imshow('Object Tracker', frame)
