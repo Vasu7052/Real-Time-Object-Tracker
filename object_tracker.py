fe69c2170b5fd831287c3959e67ae58502610cf9
Vasu Gupta <androfly.official@gmail.com>
2017-10-23 18:57:05 +0530

    Added Detect Only Once Feature

--- object_tracker.py
+++ object_tracker.py
@@ -3,6 +3,9 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)  # Webcam Capture
 
+onlyOnce = False
+alreadyDetected = [False,False,False,False,False]
+
 while (True):
 
     ret, frame = cap.read()
@@ -12,19 +15,25 @@ while (True):
         template = cv2.imread('images/pic'+str(i)+'.png', 0)
         w, h = template.shape[::-1]
 
-        res = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF_NORMED)
-        threshold = 0.6
+        if alreadyDetected[i] is False :
 
-        loc = np.where(res >= threshold)
+            res = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF_NORMED)
+            threshold = 0.6
 
-        for pt in zip(*loc[::-1]):
-            top_left = pt
-            bottom_right = (top_left[0] + w, top_left[1] + h)
-            demo = cv2.rectangle(frame, top_left, bottom_right, (0, 0, 255), 3)
-            cv2.putText(frame, 'Detected Pic: ' + str(i), (top_left[0], top_left[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1.4 , (255, 255, 255))
-            if demo is not None :
-                print('Object Found')
-            break
+            loc = np.where(res >= threshold)
+
+            for pt in zip(*loc[::-1]):
+                top_left = pt
+                bottom_right = (top_left[0] + w, top_left[1] + h)
+                demo = cv2.rectangle(frame, top_left, bottom_right, (0, 0, 255), 3)
+                if i is 1 :
+                    cv2.putText(frame, 'Mobile' , (top_left[0], top_left[1] - 10), cv2.FONT_HERSHEY_PLAIN,1.4, (255, 255, 255))
+
+                if demo is not None :
+                    if onlyOnce is True :
+                        alreadyDetected[i] = True
+                    print('Object Found')
+                break
 
 
     cv2.imshow('Object Tracker', frame)
